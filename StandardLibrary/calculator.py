a92bc3337d56867a963c7afe414658ead7ef5faa
rwofford20 <woffordr@oregonstate.edu>
2022-04-15 10:14:35 -0700

    lab2 allchanges, lab1 edits

---
+++ StandardLibrary/calculator.py
@@ -0,0 +1,18 @@
+# calculator.py
+"""Used for the Python Essentials: Standard Library lab
+<Rachel Wofford>
+<MTH 520>
+<April 14, 2020>
+"""
+from math import sqrt
+
+def sum(a,b):
+    """Returns the sum of a and b."""
+    sum = a+b
+    return(sum)
+
+def product(a,b):
+    """Returns the product of a and b."""
+    product = a*b
+    return(product)
+    
