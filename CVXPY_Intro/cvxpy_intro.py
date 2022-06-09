2a1bf6c8552bfa904a77ad095c9c80538ad9b139
rwofford20 <woffordr@oregonstate.edu>
2022-06-09 11:31:53 -0700

    finished lab 8

--- CVXPY_Intro/cvxpy_intro.py
+++ CVXPY_Intro/cvxpy_intro.py
@@ -1,10 +1,11 @@
 # cvxpy_intro.py
 """Volume 2: Intro to CVXPY.
-<Name>
-<Class>
-<Date>
+Rachel Wofford
+MTH 520 
+May 2022
 """
-
+import cvxpy as cp
+import numpy as np
 
 def prob1():
     """Solve the following convex optimization problem:
@@ -21,8 +22,24 @@ def prob1():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 1 Incomplete")
-
+    # Initialize the objective
+    x = cp.Variable(3, nonneg = True)
+    c = np.array([2, 1, 3])
+    objective = cp.Minimize(c.T @ x)
+    
+    # Write the constraints
+    A = np.array([1,2,0])
+    G = np.array([0,1,-4])
+    H = np.array([2,10,3])
+    P = np.eye(3)
+    constraints = [A @ x <= 3, G @ x <= 1, H @ x >= 12, P @ x >= 0]
+    
+    # Assemble and solve the problem
+    problem = cp.Problem(objective, constraints)
+    opt_value = problem.solve()
+    optimizer = x.value
+    
+    return optimizer, opt_value
 
 # Problem 2
 def l1Min(A, b):
@@ -39,8 +56,19 @@ def l1Min(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 2 Incomplete")
-
+    # Initialize the objective
+    x = cp.Variable(len(A.T))
+    objective = cp.Minimize(cp.norm(x,1))
+    
+    # Write the constraints
+    constraints = [A @ x == b]
+    
+    # Assemble and solve the problem
+    problem = cp.Problem(objective, constraints)
+    opt_value = problem.solve()
+    optimizer = x.value
+    
+    return optimizer, opt_value
 
 # Problem 3
 def prob3():
@@ -51,9 +79,29 @@ def prob3():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 3 Incomplete")
-
-
+    # Create the system of constraints
+    A = np.array([[1, 1, 0, 0, 0, 0], 
+                 [0, 0, 1, 1, 0, 0], 
+                 [0, 0, 0, 0, 1, 1], 
+                 [-1, 0, -1, 0, -1, 0],
+                 [0, -1, 0, -1, 0, -1]])
+    b = np.array([7, 2, 4, -5, -8]).T
+    
+    # Initialize the objective
+    x = cp.Variable(len(A.T), nonneg = True)
+    c = np.array([4, 7, 6, 8, 8, 9])
+    objective = cp.Minimize(x.T @ c)
+    
+    # Write the constraints
+    constraints = [A @ x <= b]
+    
+    # Assemble and solve the problem
+    problem = cp.Problem(objective, constraints)
+    opt_value = problem.solve()
+    optimizer = x.value
+    
+    return optimizer, opt_value
+    
 # Problem 4
 def prob4():
     """Find the minimizer and minimum of
@@ -64,8 +112,19 @@ def prob4():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 4 Incomplete")
-
+    # Initialize matrix Q and vector r
+    Q = np.array([[3, 2, 1],[2, 4, 2], [1, 2, 3]])
+    r = np.array([3, 0, 1])
+    
+    # Initialize the objective 
+    x = cp.Variable(3)
+    
+    # Assemble and solve the problem
+    problem = cp.Problem(cp.Minimize(0.5 * cp.quad_form(x, Q) + r.T @ x))
+    opt_value = problem.solve()
+    optimizer = x.value
+    
+    return optimizer, opt_value
 
 # Problem 5
 def prob5(A, b):
@@ -81,7 +140,19 @@ def prob5(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    # Initialize the objective
+    x = cp.Variable(len(A.T), nonneg = True)
+    objective = cp.Minimize(cp.norm(A @ x - b, 2))
+    
+    # Write the constraints
+    constraints = [cp.sum(x) == 1]
+    
+    # Assemble and solve the problem
+    problem = cp.Problem(objective, constraints)
+    opt_value = problem.solve()
+    optimizer = x.value
+    
+    return optimizer, opt_value
 
 
 # Problem 6
@@ -96,4 +167,23 @@ def prob6():
         The optimizer x (ndarray)
         The optimal value (float)
     """	 
+    # Optional!
     raise NotImplementedError("Problem 6 Incomplete")
+
+if __name__=="__main__":
+    # Testing problem 1
+    #print(prob1())
+    
+    # Testing problem 2
+    A = np.array([[1, 2, 1, 1], [0, 3, -2, -1]])
+    b = np.array([7,4]).T
+    #print(l1Min(A,b))
+    
+    # Testing problem 3
+    #print(prob3())
+    
+    # Testing problem 4
+    #print(prob4())
+    
+    # Testing problem 5
+    #print(prob5(A, b))
